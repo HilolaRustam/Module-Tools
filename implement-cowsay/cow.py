4528100e8785b69c0f4916695638aa194dcd1db8
HilolaRustamova <mohlaroy86@gmail.com>
2026-06-10 11:42:04 +0100

    missing cowsay file is installed

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -1,16 +1,21 @@
 import argparse
 import cowsay
 
-parser = argparse.ArgumentParser(description="Make animals say things")
+parser = argparse.ArgumentParser(
+    description="Make animals say things")
 
-parser.add_argument("message", nargs="+", help="The message to say.")
+parser.add_argument(
+    "message", nargs="+", help="The message to say.")
 
-parser.add_argument("--animal",
-choices=cowsay.char_names,
-help="The animal to be saying things.")
+parser.add_argument(
+    "--animal",
+    choices=cowsay.char_names,
+    help="The animal to be saying things.")
 
 args = parser.parse_args()
+
 message = " ".join(args.message)
-args.animal == None 
+
 animal = args.animal or "cow"
-(getattr(cowsay, animal)(message))
+
+print(getattr(cowsay, animal)(message))
