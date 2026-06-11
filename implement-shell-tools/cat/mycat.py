efadbb0babf4a9959dbbe62ab900eb3b2488be14
HilolaRustamova <mohlaroy86@gmail.com>
2026-06-11 11:13:16 +0100

    error handling corrected, structure is changed, indentation fixed

--- implement-shell-tools/cat/mycat.py
+++ implement-shell-tools/cat/mycat.py
@@ -3,7 +3,40 @@
 import sys 
 import glob
 
+def main():
+    args = sys.argv[1:]
+    
+    if not args:
+        print("Usage: cat [-n|-b] file...", file=sys.stderr)
+        sys.exit(1)
+        
+    number_all = False
+    number_nonempty = False
+    paths = []
+    
+    for a in args:
+        if a == "-n":
+            number_all = True
+        elif a == "-b":
+            number_nonempty = True
+        else:
+            paths.append(a)
+            
+    if number_nonempty:
+        number_all = False        
+            
+    files = expand_paths(paths)
+    
+    had_error = print_lines(
+        files, 
+        number_all=number_all,
+        number_nonempty=number_nonempty,
+        )
 
+    if had_error:
+        sys.exit(1)
+
+    
 def expand_paths(paths):
     """Expand glob patterns and return sorted unique file list."""
     files = []
@@ -16,16 +49,18 @@ def expand_paths(paths):
     return sorted(files)
 
 def read_lines(file):
-           with open(file, "r", encoding="utf-8") as f:
-               return f.readlines()
+    with open(file, "r", encoding="utf-8") as f:
+        return f.readlines()
        
 def print_lines(files, number_all=False, number_nonempty=False):
+    had_error = False
     line_no = 1     
     for file in files:
         try:
             lines = read_lines(file)
         except FileNotFoundError:
             print(f"cat: {file}: No such file or directory", file=sys.stderr)
+            had_error = True
             continue
         
         for line in lines:
@@ -46,31 +81,9 @@ def print_lines(files, number_all=False, number_nonempty=False):
             # avoid double newlines: line already includes '\n'
             sys.stdout.write(prefix + line)
             
+    return had_error        
             
-def main():
-    args = sys.argv[1:]
-    
-    if not args:
-        print("Usage: cat [-n|-b] file...", file=sys.stderr)
-        sys.exit(1)
-        
-    number_all = False
-    number_nonempty = False
-    paths = []
-    
-    for a in args:
-        if a == "-n":
-            number_all = True
-        elif a == "-b":
-            number_nonempty = True
-            number_all = False #-b overrides -n
-        else:
-            paths.append(a)
-            
-    files = expand_paths(paths)
-    print_lines(files, number_all,number_nonempty)
-
-    
 if __name__ == "__main__":
-    main()                                                      
+    main()      
+                                                
                     
